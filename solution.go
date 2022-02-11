b3842c3f05a0e4352797896b8b217422e7e4d518
zhmanastasiya <zhmanastasiya@mail.ru>
2022-02-12 00:17:33 +0400

    rename to solution

--- solution.go
+++ solution.go
@@ -1,14 +1,8 @@
 package solution
 
 import (
-	"fmt"
-
 	"github.com/kyokomi/emoji"
 )
-
-func main() {
-	fmt.Println(GetMessage())
-}
 
 func GetMessage() string {
 	return emoji.Sprint("Hello :world_map:!")
--- solution_test.go
+++
@@ -1,7 +0,0 @@
-package solution
-
-import "testing"
-
-func GetMessage_test(t *testing.T) {
-	t.FailNow()
-}
